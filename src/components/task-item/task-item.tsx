3fbd6878438c09a9b1849b59e25d9c3c350742f5
Ivo Sturm <14050522+ivosturm@users.noreply.github.com>
2023-10-08 22:42:05 +0200

    Update task-item.tsx
    Added mechanism to put label inside task and abbreviate if in horizontal display mode and the label is too big

--- src/components/task-item/task-item.tsx
+++ src/components/task-item/task-item.tsx
@@ -16,6 +16,8 @@ export type TaskItemProps = {
   isDelete: boolean;
   isSelected: boolean;
   rtl: boolean;
+  isHorizontalDisplay: boolean;
+  viewMode: ViewMode;
   onEventStart: (
     action: GanttContentMoveAction,
     selectedTask: BarTask,
@@ -31,11 +33,14 @@ export const TaskItem: React.FC<TaskItemProps> = props => {
     taskHeight,
     isSelected,
     rtl,
+    isHorizontalDisplay,
+    viewMode,
     onEventStart,
   } = {
     ...props,
   };
   const textRef = useRef<SVGTextElement>(null);
+  const displayNameShortenedRef = useRef(null);
   const [taskItem, setTaskItem] = useState<JSX.Element>(<div />);
   const [isTextInside, setIsTextInside] = useState(true);
 
@@ -57,10 +62,34 @@ export const TaskItem: React.FC<TaskItemProps> = props => {
   }, [task, isSelected]);
 
   useEffect(() => {
-    if (textRef.current) {
-      setIsTextInside(textRef.current.getBBox().width < task.x2 - task.x1);
+    // Ivo Sturm: added feature if displayName is too large, shortens the text to make it fit in the bar and adds 3 bullets to let user know it is shortened
+    let isTextInside = true;
+    // Check 1: If task bar size is less than 30, don't put any text in
+    if ((task.x2 - task.x1) < 30){
+      isTextInside = false;
+      displayNameShortenedRef.current = null;
     }
-  }, [textRef, task]);
+    else if (textRef.current) {
+      // Check 2: At first render the textRef bounding box will reflect the actual size of the displayname. Check whether this fits in the width of the bar of the task
+      const deltaWidth = textRef.current.getBBox().width - (task.x2 - task.x1);
+      if (Math.floor(deltaWidth / 6) >=1){
+        // remove 3 extra characters, since we will add three dots to let user know name is longer than displayed
+        const charsToBeRemoved = Math.floor(deltaWidth / 6) + 3;
+        const displayNameShortened = task.displayName.substring(0, task.displayName.length - charsToBeRemoved); 
+       // Check 3: if corrected text is smaller than 6 don't show text inside either...
+        if (displayNameShortened.length > 5){
+          displayNameShortenedRef.current = displayNameShortened;
+        } else {
+          isTextInside = false;
+          displayNameShortenedRef.current = null;
+        }       
+      } else {
+        displayNameShortenedRef.current = null;
+      }           
+    } 
+    setIsTextInside(isTextInside);
+    // Ivo Sturm: added viewMode in dependencies, to rerender once view mode changes
+  }, [textRef, task, viewMode]);
 
   const getX = () => {
     const width = task.x2 - task.x1;
@@ -79,7 +108,10 @@ export const TaskItem: React.FC<TaskItemProps> = props => {
       return task.x1 + width + arrowIndent * +hasChild + arrowIndent * 0.2;
     }
   };
-
+  // Ivo Sturm: introduced showBartext boolean. Don't show the bar if it should be displayed horizontally and the text is pushed outside of the bar because it is too large
+  // If the label can't be shown in horizontal mode it is important to still generate it, else the textRef will be gone and in the next View Mode selection
+  // it can't be determined anymore whether the label fits inside the text box as the reference is gone..
+  var showBarText = !(isHorizontalDisplay && !isTextInside);
   return (
     <g
       onKeyDown={e => {
@@ -116,9 +148,10 @@ export const TaskItem: React.FC<TaskItemProps> = props => {
             ? style.barLabel
             : style.barLabel && style.barLabelOutside
         }
+        style={showBarText ? {} : {"visibility":"hidden"}}
         ref={textRef}
       >
-        {task.name}
+        {displayNameShortened}
       </text>
     </g>
   );
